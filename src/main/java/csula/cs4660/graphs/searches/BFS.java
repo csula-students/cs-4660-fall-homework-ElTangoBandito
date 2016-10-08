182f19aabd364c01ccdd28186fe65b9808794a83
ElTangoBandito <Volfurious@gmail.com>
2016-10-07 18:23:51 -0700

    Finished BFS?

--- src/main/java/csula/cs4660/graphs/searches/BFS.java
+++ src/main/java/csula/cs4660/graphs/searches/BFS.java
@@ -1,14 +1,11 @@
 package csula.cs4660.graphs.searches;
 
+import com.google.common.collect.Lists;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
-import java.util.List;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Breadth first search
@@ -16,9 +13,12 @@ import java.util.Stack;
 public class BFS implements SearchStrategy {
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
+        System.out.println("running??");
+        Map<Node, Node> nodeMap = new HashMap<Node, Node>();
         Queue<Node> nodeQueue = new LinkedList<Node>();
         List<Node> visited = new ArrayList<Node>();
         List<Edge> result = new ArrayList<Edge>();
+
         //Queue<Edge> accum = new LinkedList<Edge>();
         nodeQueue.add(source);
         while (!nodeQueue.isEmpty()){
@@ -29,18 +29,44 @@ public class BFS implements SearchStrategy {
             }
             if (!visited.contains(currentNode)){
                 visited.add(currentNode);
+                //System.out.println(currentNode);
                 List<Node> childs = graph.neighbors(currentNode);
                 for (Node node: childs){
-                    if (!visited.contains(node)){
-                        nodeQueue.add(node);
+                    nodeMap.put(node, currentNode);
+                    if(node.equals(dist)){
+                        nodeQueue.clear();
+                        break;
                     }
+                    nodeQueue.add(node);
                 }
             }
         }
-        System.out.println(visited);
 
+        //System.out.println(nodeMap);
+
+        Node tracerNode = dist;
+        while(!tracerNode.equals(source)){
+            Node parentNode = nodeMap.get(tracerNode);
+            System.out.println(graph.distance(parentNode, tracerNode));
+            Edge e = new Edge(parentNode, tracerNode, graph.distance(parentNode, tracerNode));
+            result.add(e);
+            tracerNode = parentNode;
+        }
+        System.out.println("Whyyyy");
+        //result = Lists.reverse(result);
+        return result;
+        //return Lists.reverse(result);
+        /*
+        //reverse result
+        List<Edge> realResult = new ArrayList<Edge>();
+        for(int i = result.size() - 1; i > -1; i--){
+            realResult.add(result.get(i));
+        }
 
         //graph.recursiveBFS(source, dist, nodeQueue, visited, result, accum);
-        return result;
+        return realResult;
+        */
+
     }
+
 }
